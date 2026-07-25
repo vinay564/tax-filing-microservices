09c889b1e254fb89543e61d6f556064f35c5e35e
Vinay_M <vinay2201.manavarthi@gmail.com>
2026-07-25 18:00:13 -0400

    Fix KafkaOperations generic type mismatch in payment-service error handler

--- payment-service/src/main/java/com/uitax/payment/config/KafkaErrorHandlingConfig.java
+++ payment-service/src/main/java/com/uitax/payment/config/KafkaErrorHandlingConfig.java
@@ -4,6 +4,7 @@ import org.apache.kafka.common.TopicPartition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.KafkaOperations;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
 import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.util.backoff.FixedBackOff;
@@ -12,15 +13,13 @@ import org.springframework.util.backoff.FixedBackOff;
 public class KafkaErrorHandlingConfig {
 
     @Bean
-    public DefaultErrorHandler errorHandler(KafkaOperations<Object, Object> kafkaTemplate) {
+    public DefaultErrorHandler errorHandler(KafkaTemplate<Object, Object> kafkaTemplate) {
+        KafkaOperations<Object, Object> operations = kafkaTemplate;
 
-        DeadLetterPublishingRecoverer recoverer = new DeadLetterPublishingRecoverer(kafkaTemplate,
+        DeadLetterPublishingRecoverer recoverer = new DeadLetterPublishingRecoverer(operations,
                 (record, exception) -> new TopicPartition(record.topic() + ".DLT", record.partition()));
-
         FixedBackOff backOff = new FixedBackOff(1000L, 3L);
-
         DefaultErrorHandler errorHandler = new DefaultErrorHandler(recoverer, backOff);
-
         return errorHandler;
     }
 }
